f3ef94114d169fc5f5a30ec02f586890c3be62f8
narenak-sec <nrwed3158@gmail.com>
2026-04-19 22:36:58 -0400

    Initial Project structure with README, config, add .gitignore

---
+++ README.md
@@ -0,0 +1,7 @@
+# FreshBasket Customer Churn Prediction
+
+Predicting 30-day customer churn using transaction, feedback, and app usage data.
+
+## Team
+- You (ML Engineer)
+- Arjun (ML Engineer)
---
+++ src/train.py
@@ -0,0 +1,5 @@
+# FreshBasket Churn Model - Training Script
+import pandas as pd
+from sklearn.linear_model import LogisticRegression echo 
+from sklearn.linear_model import LogisticRegression echo 
+print('Training script placeholder')
